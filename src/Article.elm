3155c437090123b449d4f14429a249ec9b0130cf
flupsi <upsiflu@gmail.com>
2020-05-21 02:02:56 +0200

    Refactoring

--- src/Article.elm
+++ src/Article.elm
@@ -1,53 +1,58 @@
-module Article exposing 
-    ( Article, Format, Draft, Caret, Msg, Appearance
-    -- create
+module Article exposing
+    (  Appearance(..)
+       -- create
+
+    , Article
+    , Caret
+    , Draft
+    , Format
+    , Msg
     , singleton
-    -- from Editor, via Mosaic:
     , update
-    -- from Mosaic:
-    {-, got-}
     , view
     )
 
-
-import Tile
-
-import Html exposing (Html, div, strong, text, span, pre)
-import Html.Attributes exposing (class, attribute)
-import Html.Events exposing (on, onDoubleClick, onBlur)
-import Ui exposing (..)
-import Json.Decode as Decode exposing (Decoder, string, list)
+import Html exposing (Html, div, pre, span, strong, text)
+import Html.Attributes exposing (attribute, class)
+import Html.Events exposing (on, onBlur, onDoubleClick)
+import Json.Decode as Decode exposing (Decoder, list, string)
 import Json.Decode.Pipeline exposing (requiredAt)
-
-
+import Tile
+import Ui exposing (..)
 
 
 type alias Article =
-   -- the text that the vDOM thinks is current:
-   { release : Release  
-   -- the most recent formatting command:
-   , format : Format
+    -- the text that the vDOM thinks is current:
+    { release : Release
 
-   -- the text that the user is actually editing:
-   , draft : Draft
-   , caret : Caret
-   }
+    -- the most recent formatting command:
+    , format : Format
+
+    -- the text that the user is actually editing:
+    , draft : Draft
+    , caret : Caret
+    }
+
 
 type Caret
-    = Caret ( List String )
+    = Caret (List String)
 
-type alias Format
-    = String
+
+type alias Format =
+    String
+
 
 type Draft
     = Draft String
 
-type alias Release
-    = String
+
+type alias Release =
+    String
 
 
 
 -- generate
+
 
 singleton : String -> Article
 singleton s =
@@ -58,28 +63,33 @@ singleton s =
     }
 
 
+
 {-
 
-got : Tile.Message -> Article -> Article
-got message article =
-    case ( message, article.editing ) of
-        ( Tile.WalkedAway, True ) ->
-            { article | editing = False
-                      , release = article.draft |> \(Draft string) -> string 
-            }
-        ( Tile.WalkedHere, False ) ->
-            { article | editing = True }
+   got : Tile.Message -> Article -> Article
+   got message article =
+       case ( message, article.editing ) of
+           ( Tile.WalkedAway, True ) ->
+               { article | editing = False
+                         , release = article.draft |> \(Draft string) -> string
+               }
+           ( Tile.WalkedHere, False ) ->
+               { article | editing = True }
 
-        _ -> article
+           _ -> article
 -}
+
 
 type Msg
     = DraftChanged Draft
-    | CaretChanged Caret  
+    | CaretChanged Caret
     | FormatIssued Format
-    {-
-    | TileMessage Tile.Message
-    -}
+
+
+
+{-
+   | TileMessage Tile.Message
+-}
 
 
 update : Msg -> Article -> Article
@@ -87,124 +97,142 @@ update msg article =
     case msg of
         DraftChanged new ->
             { article | draft = new }
+
         CaretChanged new ->
             { article | caret = new }
+
         FormatIssued new ->
             { article | format = new }
-  {-
-        TileMessage message ->
-            case ( message, article.editing ) of
-                ( Tile.WalkedAway, True ) ->
-                    { article 
-                        | editing = False
-                        , release = article.draft |> \(Draft string) -> string 
-                    }
-                ( Tile.WalkedHere, False ) ->
-                    { article 
-                        | editing = True 
-                    }
-                _ -> article
+
+
+
+{-
+   TileMessage message ->
+       case ( message, article.editing ) of
+           ( Tile.WalkedAway, True ) ->
+               { article
+                   | editing = False
+                   , release = article.draft |> \(Draft string) -> string
+               }
+           ( Tile.WalkedHere, False ) ->
+               { article
+                   | editing = True
+               }
+           _ -> article
 
 -}
-
 -- decoders
+
 
 draftDecoder : Decoder Draft
 draftDecoder =
     Decode.succeed Draft
         |> requiredAt [ "detail", "draft" ] string
 
+
 caretDecoder : Decoder Caret
 caretDecoder =
     Decode.succeed Caret
-        |> requiredAt [ "detail", "caret" ] ( list string )
-
+        |> requiredAt [ "detail", "caret" ] (list string)
 
 
 type Appearance msg
     = Normal
     | Selected
-    | Editor  ( Msg -> msg )
+    | Editor (Msg -> msg)
+
 
 
 -- Mosaic constructs the Appearance viewModel.
 
+
 view : Appearance msg -> Article -> Ui msg
 view appearance article =
-    let decodeOn event decoder message =
+    let
+        decodeOn event decoder message =
             decoder
-            |> Decode.map message
-            |> on event
-        
-        toolbar_placeholder = div [ class "toolbar "] [] 
+                |> Decode.map message
+                |> on event
 
-    in case appearance of
+        toolbar_placeholder =
+            div [ class "toolbar " ] []
+    in
+    case appearance of
         Normal ->
             Ui.Static
-            [ toolbar_placeholder
-            , Html.node "custom-editor"
-                [ attribute "release" article.release
-                , attribute "format" article.format
-                , attribute "state" "done"
+                [ toolbar_placeholder
+                , Html.node "custom-editor"
+                    [ attribute "release" article.release
+                    , attribute "format" article.format
+                    , attribute "state" "done"
+                    ]
+                    []
                 ]
-                []
-            ]
+
         Selected ->
             Ui.Static
-            [ toolbar_placeholder
-            , Html.node "custom-editor"
-                [ attribute "release" article.release
-                , attribute "format" article.format
-                , attribute "state" "done"
+                [ toolbar_placeholder
+                , Html.node "custom-editor"
+                    [ attribute "release" article.release
+                    , attribute "format" article.format
+                    , attribute "state" "done"
+                    ]
+                    []
                 ]
-                []
-            ]
+
         Editor how_to_message ->
             Ui.Active
-            [ viewToolbar ( FormatIssued >> how_to_message ) article.caret
-            , Html.node "custom-editor"
-                [ attribute "release" article.release
-                , attribute "format" article.format
-                , attribute "state" "editing"
-                , decodeOn "draft" draftDecoder ( DraftChanged >> how_to_message  )
-                , decodeOn "caret" caretDecoder ( CaretChanged >> how_to_message  )
+                [ viewToolbar (FormatIssued >> how_to_message) article.caret
+                , Html.node "custom-editor"
+                    [ attribute "release" article.release
+                    , attribute "format" article.format
+                    , attribute "state" "editing"
+                    , decodeOn "draft" draftDecoder (DraftChanged >> how_to_message)
+                    , decodeOn "caret" caretDecoder (CaretChanged >> how_to_message)
+                    ]
+                    []
                 ]
-                []
-            ]
 
 
-
-viewToolbar : ( Format -> msg ) -> Caret -> Html msg
-viewToolbar on_format ( Caret caret ) =
+viewToolbar : (Format -> msg) -> Caret -> Html msg
+viewToolbar on_format (Caret caret) =
     let
         is_in : List a -> a -> Bool
-        is_in l a = List.member a l
-        
-        
+        is_in l a =
+            List.member a l
+
         toggle : String -> String -> String -> Ui.Face -> Ui.Option msg
         toggle command revert match face =
             face
                 |> Ui.Toggle
-                |> \needs_toggle_and_is_on -> decideState command revert match |> needs_toggle_and_is_on
+                |> (\needs_toggle_and_is_on -> decideState command revert match |> needs_toggle_and_is_on)
 
         decideState on off =
-            is_in caret >> \activated->
-                { toggle = on_format 
-                    ( if activated then off else on )
-                , is_on = activated }
+            is_in caret
+                >> (\activated ->
+                        { toggle =
+                            on_format
+                                (if activated then
+                                    off
 
+                                 else
+                                    on
+                                )
+                        , is_on = activated
+                        }
+                   )
     in
-    [ Ui.icon "Numbered list"           "format_list_numbered"   |> toggle "makeOrderedList"    "removeList" "OL"
-    , Ui.icon "List with bullet points" "format_list_bulleted"   |> toggle "makeUnorderedList"  "removeList" "UL"
-    , Ui.icon "Decrease Level"          "format_indent_decrease" |> toggle "decreaseLevel"      "increaseListLevel" "neverj"
-    , Ui.icon "Increase Level"          "format_indent_increase" |> toggle "increaseLevel"      "decreaseListLevel" "never"
-    , Ui.preview "Title"             "h1" [ Ui.text "Title" ]    |> toggle "makeTitle"          "removeHeader" "H1"
-    , Ui.preview "Chapter Heading"   "h2" [ Ui.text "Heading"]   |> toggle "makeHeader"         "removeHeader" "H2"
-    , Ui.preview "Secondary Heading" "h3" [ Ui.text "Secondary"] |> toggle "makeSubheader"      "removeHeader" "H3"
-    , Ui.preview "Strong emphasis"  "T b" [ Ui.text "B" ]        |> toggle "bold"               "removeBold" "b"
-    , Ui.preview "Emphasis"         "T i" [ Ui.text "I" ]        |> toggle "italic"             "removeItalic" "i"
-    , Ui.icon "Clear formatting"        "format_clear"           |> toggle "removeAllFormatting" "" "?"
-    , Ui.icon "Hyperlink"               "link"                   |> toggle "addLink"            "removeLink" "a"
-    , Ui.icon "Clear hyperlink"         "link_off"               |> toggle "removeLink"         "" "?"
-    ] 
-    |> Ui.viewToolbar "paragraph-style"
+    [ Ui.icon "Numbered list" "format_list_numbered" |> toggle "makeOrderedList" "removeList" "OL"
+    , Ui.icon "List with bullet points" "format_list_bulleted" |> toggle "makeUnorderedList" "removeList" "UL"
+    , Ui.icon "Decrease Level" "format_indent_decrease" |> toggle "decreaseLevel" "increaseListLevel" "neverj"
+    , Ui.icon "Increase Level" "format_indent_increase" |> toggle "increaseLevel" "decreaseListLevel" "never"
+    , Ui.preview "Title" "h1" [ Ui.text "Title" ] |> toggle "makeTitle" "removeHeader" "H1"
+    , Ui.preview "Chapter Heading" "h2" [ Ui.text "Heading" ] |> toggle "makeHeader" "removeHeader" "H2"
+    , Ui.preview "Secondary Heading" "h3" [ Ui.text "Secondary" ] |> toggle "makeSubheader" "removeHeader" "H3"
+    , Ui.preview "Strong emphasis" "T b" [ Ui.text "B" ] |> toggle "bold" "removeBold" "b"
+    , Ui.preview "Emphasis" "T i" [ Ui.text "I" ] |> toggle "italic" "removeItalic" "i"
+    , Ui.icon "Clear formatting" "format_clear" |> toggle "removeAllFormatting" "" "?"
+    , Ui.icon "Hyperlink" "link" |> toggle "addLink" "removeLink" "a"
+    , Ui.icon "Clear hyperlink" "link_off" |> toggle "removeLink" "" "?"
+    ]
+        |> Ui.viewToolbar "paragraph-style"
--- src/Zip.elm
+++ src/Zip.elm
@@ -1,27 +1,48 @@
-module Zip exposing 
-  (Path(..), IntPath, Wing(..), Zip, focus
-    , foldl_left_side, foldl_right_side
-    , foldl_homogenous, fold_homogenous, foldr_homogenous
-    , foldr_left_side, foldr_right_side
-    , foldl, fold, foldr
-    , insert_left, insert_right, left, length
-    , map_periphery, map_focus
-    , indexed_map_periphery, indexed_map_focus
-    , path_to_string, path_to_int
+module Zip exposing
+    ( IntPath
+    , Path(..)
+    , Wing(..)
+    , Zip
+    , focus
+    , fold
+    , fold_homogenous
+    , foldl
+    , foldl_homogenous
+    , foldl_left_side
+    , foldl_right_side
+    , foldr
+    , foldr_homogenous
+    , foldr_left_side
+    , foldr_right_side
+    , indexed_map_focus
+    , indexed_map_periphery
+    , insert_left
+    , insert_right
     , int_to_path
-    , reverse, right, singleton
-    , to_string, walk)
-
+    , left
+    , length
+    , map_focus
+    , map_periphery
+    , path_to_int
+    , path_to_string
+    , reverse
+    , right
+    , singleton
+    , to_string
+    , walk
+    )
 
 {- A Zipper over a List.
 
-Use this structure to model a collection of linearly connected items where one (the "focus") is special,
-and where you can walk left and right to make another one special.
+   Use this structure to model a collection of linearly connected items where one (the "focus") is special,
+   and where you can walk left and right to make another one special.
 
- - wraps on the left and right edges; thus every path is legal.
- - focus and periphery have distinct data types.
+    - wraps on the left and right edges; thus every path is legal.
+    - focus and periphery have distinct data types.
 
 -}
+
+
 type Zip c a
     = Zip (List a) c (List a)
 
@@ -31,7 +52,10 @@ type Path
     | R Path
     | Here
 
-type alias IntPath = Int
+
+type alias IntPath =
+    Int
+
 
 type Wing
     = Left
@@ -63,46 +87,66 @@ insert_left a (Zip l c r) =
     Zip (a :: l) c r
 
 
+
 -- wrap around the edges.
-left : ( c -> a ) -> ( a -> c ) -> Zip c a -> Zip c a
+
+
+left : (c -> a) -> (a -> c) -> Zip c a -> Zip c a
 left ca ac ((Zip l c r) as original) =
     case ( l, List.reverse r ) of
-        ( [], [] ) -> 
+        ( [], [] ) ->
             original
-        ( [], ri::ght ) ->
-            Zip ( ght++[ ca c] ) ( ac ri ) []
-        ( le::ft, _ ) ->
-            Zip ft ( ac le ) ( ca c ::r )
 
-right : ( c -> a ) -> ( a -> c ) -> Zip c a -> Zip c a
+        ( [], ri :: ght ) ->
+            Zip (ght ++ [ ca c ]) (ac ri) []
+
+        ( le :: ft, _ ) ->
+            Zip ft (ac le) (ca c :: r)
+
+
+right : (c -> a) -> (a -> c) -> Zip c a -> Zip c a
 right ca ac ((Zip l c r) as original) =
     case ( List.reverse l, r ) of
-        ( [], [] ) -> 
+        ( [], [] ) ->
             original
-        ( le::ft, [] ) ->
-            Zip [] ( ac le ) ( ft++[ ca c ])
-        ( _, ri::ght ) ->
-            Zip ( ca c::l ) ( ac ri ) ght
 
-map_focus : ( c -> d ) -> Zip c a -> Zip d a
+        ( le :: ft, [] ) ->
+            Zip [] (ac le) (ft ++ [ ca c ])
+
+        ( _, ri :: ght ) ->
+            Zip (ca c :: l) (ac ri) ght
+
+
+map_focus : (c -> d) -> Zip c a -> Zip d a
 map_focus cd (Zip l c r) =
     Zip l (cd c) r
 
-indexed_map_focus : ( Int -> c -> d ) -> Zip c a -> Zip d a
+
+indexed_map_focus : (Int -> c -> d) -> Zip c a -> Zip d a
 indexed_map_focus icd (Zip l c r) =
     Zip l (icd 0 c) r
 
-map_periphery : ( a -> b ) -> Zip c a -> Zip c b
+
+map_periphery : (a -> b) -> Zip c a -> Zip c b
 map_periphery ab (Zip l c r) =
     Zip (List.map ab l) c (List.map ab r)
 
-indexed_map_periphery : ( Int -> a -> b ) -> Zip c a -> Zip c b
+
+indexed_map_periphery : (Int -> a -> b) -> Zip c a -> Zip c b
 indexed_map_periphery iab (Zip l c r) =
-    Zip (List.indexedMap ( negate >> iab ) l) c (List.indexedMap iab r)
+    Zip (List.indexedMap (negate >> iab) l) c (List.indexedMap iab r)
+
 
 foldl : { focus : c -> b -> b, periphery : a -> b -> b } -> b -> Zip c a -> b
 foldl fu acc (Zip l c r) =
     List.foldl fu.periphery acc l
+        |> fu.focus c
+        |> (\done -> List.foldl fu.periphery done r)
+
+
+fold : { focus : c -> b -> b, periphery : a -> b -> b } -> b -> Zip c a -> b
+fold fu acc (Zip l c r) =
+    List.foldr fu.periphery acc l
         |> fu.focus c
         |> (\done -> List.foldl fu.periphery done r)
 
@@ -112,32 +156,47 @@ foldr fu acc =
     reverse >> foldl fu acc
 
 
-foldl_left_side : ( a -> b -> b ) -> b -> Zip c a -> b
-foldl_left_side acc primer (Zip l _ _) = List.foldl acc primer l
+foldl_left_side : (a -> b -> b) -> b -> Zip c a -> b
+foldl_left_side acc primer (Zip l _ _) =
+    List.foldl acc primer l
 
-foldl_right_side : ( a -> b -> b ) -> b -> Zip c a -> b
-foldl_right_side acc primer (Zip _ _ r) = List.foldl acc primer r
 
-foldr_left_side : ( a -> b -> b ) -> b -> Zip c a -> b
-foldr_left_side acc primer (Zip l _ _) = List.foldr acc primer l
+foldl_right_side : (a -> b -> b) -> b -> Zip c a -> b
+foldl_right_side acc primer (Zip _ _ r) =
+    List.foldl acc primer r
 
-foldr_right_side : ( a -> b -> b ) -> b -> Zip c a -> b
-foldr_right_side acc primer (Zip _ _ r) = List.foldr acc primer r
 
-foldl_homogenous : ( a -> b -> b ) -> b -> Zip a a -> b
+foldr_left_side : (a -> b -> b) -> b -> Zip c a -> b
+foldr_left_side acc primer (Zip l _ _) =
+    List.foldr acc primer l
+
+
+foldr_right_side : (a -> b -> b) -> b -> Zip c a -> b
+foldr_right_side acc primer (Zip _ _ r) =
+    List.foldr acc primer r
+
+
+foldl_homogenous : (a -> b -> b) -> b -> Zip a a -> b
 foldl_homogenous fu =
     foldl { focus = fu, periphery = fu }
 
-foldr_homogenous : ( a -> b -> b ) -> b -> Zip a a -> b
+
+fold_homogenous : (a -> b -> b) -> b -> Zip a a -> b
+fold_homogenous fu =
+    fold { focus = fu, periphery = fu }
+
+
+foldr_homogenous : (a -> b -> b) -> b -> Zip a a -> b
 foldr_homogenous fu =
     foldr { focus = fu, periphery = fu }
+
 
 reverse : Zip c a -> Zip c a
 reverse (Zip l c r) =
     Zip (List.reverse r) c (List.reverse l)
 
 
-walk : ( c -> a ) -> ( a -> c ) -> Path -> Zip c a -> Zip c a
+walk : (c -> a) -> (a -> c) -> Path -> Zip c a -> Zip c a
 walk ca ac p =
     case p of
         Here ->
@@ -175,11 +234,17 @@ path_to_int p =
         R rest ->
             path_to_int rest + 1
 
+
 int_to_path : IntPath -> Path
 int_to_path i =
-    if      ( i < 0 ) then L <| int_to_path ( i+1 )
-    else if ( i > 0 ) then R <| int_to_path ( i-1 )
-    else    Here
+    if i < 0 then
+        L <| int_to_path (i + 1)
+
+    else if i > 0 then
+        R <| int_to_path (i - 1)
+
+    else
+        Here
 
 
 to_string : Zip c a -> String
